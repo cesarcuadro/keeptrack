46fe9d8e95866e81de7148add922b1c380407ccc
cesarcuadro <cesarcuadro777@gmail.com>
2023-02-21 15:52:29 -0800

    hiding edit form component

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
@@ -8,8 +8,9 @@ interface ProjectListProps {
 }
 
 const ProjectList = ({ projects }: ProjectListProps) => {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
   const handleEdit = (project: Project) => {    
-    console.log(project);
+    setProjectBeingEdited(project);
   };
   return (
     <ul className="row">
@@ -17,8 +18,11 @@ const ProjectList = ({ projects }: ProjectListProps) => {
         <div className="row">
           {projects.map((project) => (
             <div key={project.id} className="cols-sm">
-              <ProjectCard project={project} onEdit={handleEdit}></ProjectCard>
-              <ProjectForm />
+              {project === projectBeingEdited ? (
+                <ProjectForm />
+              ) : (
+              <ProjectCard project={project} onEdit={handleEdit} />
+          )}
             </div>
           ))}
         </div>
