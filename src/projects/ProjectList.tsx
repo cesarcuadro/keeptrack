277f547e22c2e5455b439ed24228ac15a2b3e7ca
cesarcuadro <cesarcuadro777@gmail.com>
2023-02-21 14:57:04 -0800

    displayed list data with css

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -7,7 +7,26 @@ interface ProjectListProps {
 
 const ProjectList = ({ projects }: ProjectListProps) => {
   return (
-    <pre>{JSON.stringify(projects, null, ' ')}</pre>
+    <ul className="row">
+      {projects.map((project) => (
+        <div className="row">
+          {projects.map((project) => (
+            <div key={project.id} className="cols-sm">
+              <div className="card">
+                <img src={project.imageUrl} alt={project.name} />
+                <section className="section dark">
+                  <h5 className="strong">
+                    <strong>{project.name}</strong>
+                  </h5>
+                  <p>{project.description}</p>
+                  <p>Budget : {project.budget.toLocaleString()}</p>
+                </section>
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </ul>
   )
 }
 
