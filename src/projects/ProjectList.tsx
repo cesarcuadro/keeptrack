be101d358539ab0977d2eb8bf311c6cf2e21f6a6
cesarcuadro <cesarcuadro777@gmail.com>
2023-02-21 14:47:09 -0800

    passing data through a component

---
+++ src/projects/ProjectList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Project } from './Project';
+
+interface ProjectListProps {
+    projects: Project[];
+}
+
+const ProjectList = ({ projects }: ProjectListProps) => {
+  return (
+    <pre>{JSON.stringify(projects, null, ' ')}</pre>
+  )
+}
+
+export default ProjectList
