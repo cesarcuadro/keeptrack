011c40616f2d3113047862b9c5ce1e1239f65ca8
cesarcuadro <cesarcuadro777@gmail.com>
2023-02-21 17:28:25 -0800

    added pagination - put more btn at btm of pg

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -7,14 +7,22 @@ const ProjectsPage = () => {
   const [projects, setProjects] = useState<Project[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+  const handleMoreClick = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
 
   useEffect(() => {
     async function loadProjects() {
       setLoading(true);
       try {
-        const data = await projectAPI.get(1);
+        const data = await projectAPI.get(currentPage);
         setError('');
-        setProjects(data);
+        if (currentPage === 1) {
+          setProjects(data);
+        } else {
+          setProjects((projects) => [...projects, ...data]);
+        }
       }
        catch (e) {
         if (e instanceof Error) {
@@ -25,7 +33,7 @@ const ProjectsPage = () => {
       }
     }
     loadProjects();
-  }, []);
+  }, [currentPage]);
 
   const saveProject = (project: Project) => {
     let updatedProjects = projects.map((p: Project) => {
@@ -52,6 +60,17 @@ const ProjectsPage = () => {
       )}
 
       <ProjectList onSave={saveProject} projects={projects} />
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More...
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       {loading && (
         <div className="center-page">
           <span className="spinner primary"></span>
