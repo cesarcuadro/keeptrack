d89cc24684614cfb46b8a69fe986f23b908980ed
cesarcuadro <cesarcuadro777@gmail.com>
2023-02-27 17:22:51 -0800

    added material ui card component

--- src/projects/ProjectDetail.tsx
+++ src/projects/ProjectDetail.tsx
@@ -1,36 +1,42 @@
 import React from 'react';
 import { Project } from './Project';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 interface ProjectDetailProps {
   project: Project;
 }
+
 export default function ProjectDetail({ project }: ProjectDetailProps) {
   return (
-    <div className="row">
-      <div className="col-sm-6">
-        <div className="card large">
-          <img
-            className="rounded"
-            src={project.imageUrl}
-            alt={project.name}
-          />
-          <section className="section dark">
-            <h3 className="strong">
-              <strong>{project.name}</strong>
-            </h3>
-            <p>{project.description}</p>
-            <p>Budget : {project.budget}</p>
-
-            <p>Signed: {project.contractSignedOn.toLocaleDateString()}</p>
-            <p>
-              <mark className="active">
-                {' '}
-                {project.isActive ? 'active' : 'inactive'}
-              </mark>
-            </p>
-          </section>
-        </div>
-      </div>
-    </div>
+    <Card sx={{ maxWidth: 600 }}>
+      <CardMedia
+        component="img"
+        height="400"
+        image={project.imageUrl}
+        alt={project.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {project.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {project.description}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Budget : {project.budget}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Signed: {project.contractSignedOn.toLocaleDateString()}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          <mark className="active">
+            {project.isActive ? 'active' : 'inactive'}
+          </mark>
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
