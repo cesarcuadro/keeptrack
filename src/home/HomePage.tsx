dff797e63ccb1b71ac4ecda33438292ad4abc700
cesarcuadro <cesarcuadro777@gmail.com>
2023-03-02 11:14:53 -0800

    put bg img onto home pg

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -3,8 +3,19 @@ import React from 'react'
 const HomePage = () => {
   return (
     <>
+    <div style={{
+      backgroundImage: "url(assets/bgDream.png)",
+      backgroundRepeat: "no-repeat",
+        backgroundSize: "100%, 100%",
+        objectFit: "cover",
+        height: "100%",
+        width: "100%",
+        paddingTop: 6,
+        textAlign: "center",
+      }}>
       <h2>Welcome to Acme Co</h2>
       <p>Paragraph</p>
+      </div>
     </>
   )
 }
