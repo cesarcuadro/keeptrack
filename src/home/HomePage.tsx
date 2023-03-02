28e741e2bbc73dc7bceca9edff7e95337b1dfdf6
cesarcuadro <cesarcuadro777@gmail.com>
2023-03-02 13:52:59 -0800

    put in content for home pg

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -13,9 +13,18 @@ const HomePage = () => {
         paddingTop: 6,
         textAlign: "center",
       }}>
-        <div>
+        <div style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          marginTop: "10px",
+          border: "solid",
+          margin: '10px',
+          backgroundColor: "whitesmoke",
+          opacity: "50%"
+          }}>
           <h2>Welcome to Acme Co</h2>
-          <p>Paragraph</p>
+          <p>Acme Co is a project management company specializing in commercial real estate. Our team of experienced professionals provides comprehensive project management services, including planning, budgeting, scheduling, and oversight, to ensure successful and timely completion of your commercial real estate projects. With a focus on quality, efficiency, and client satisfaction, we are committed to delivering exceptional results that meet your business objectives. Let us help you bring your commercial real estate projects to life.</p>
       </div>
       </div>
     </>
